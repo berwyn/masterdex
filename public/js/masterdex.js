15e927d711828e307a88280be5595d82073f0f5c
Berwyn Codeweaver <berwyn.codeweaver@gmail.com>
2013-12-30 00:20:10 -0700

    Better homepage

--- public/js/masterdex.js
+++ public/js/masterdex.js
@@ -7,11 +7,16 @@ var pokeball,
 
 window.onload = function() {
   pokeball = document.getElementById('pokeball');
+  if(window.location.pathname === "/") {
+    $('footer').addClass('home-footer');
+  }
 };
 
 step = function(timestamp) {
   var delta = timestamp - start,
       deg = (360 * (delta/1000)) % 360;
+  pokeball.style.transform = "rotate("+deg+"deg)";
+  pokeball.style.mozTransform = "rotate("+deg+"deg)";
   pokeball.style.webkitTransform = "rotate("+deg+"deg)";
   animation = requestAnimationFrame(step);
 }
