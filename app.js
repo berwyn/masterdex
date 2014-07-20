94b9d792bf846a4cf6dbaa69d3c51b3437616c04
berwyn <berwyn.codeweaver@gmail.com>
2014-07-20 01:41:47 -0600

    Start controller work (with dummy data for now)

--- app.js
+++ app.js
@@ -1,50 +1,35 @@
 'use strict';
 
-var MEDIA_TYPES = ['application/json', 'text/html'];
-
 var express 	= require('express'),
 	hbs			= require('hbs'),
 	hbsutils	= require('hbs-utils')(hbs),
-	less		= require('less-middleware'),
 	Negotiator	= require('negotiator'),
 	bodyParser	= require('body-parser'),
+	controllers = require('./controllers'),
 	app			= express(),
 	router  	= express.Router(),
 	port		= process.env.PORT || 8080;
 
+// Configure
 app.set('port', port);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-app.use(less(__dirname + '/static'));
+// Middleware
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser());
 app.use(function(req, res, next) {
-	app.locals.meta = { path: req.path };
+	req.locals.meta = { path: req.path };
 	next();
 });
+app.use(router);
 
-app.use(function(req, res, next) {
-	next();
-	if(res.locals.entity) {
-		var mediaType = new Negotiator(req).mediaType(MEDIA_TYPES);
-		switch(mediaType) {
-			case 'application/json':
-				res.send(JSON.stringify(res.locals.entity));
-				break;
-			case 'text/html':
-			default:
-				res.render(res.locals.template);
-				break;
-		}
-	}
-})
-
-hbs.localsAsTemplateData(app);
+// Handlebars helpers
 hbs.registerHelper('eq', function(first, second, options) {
 	return (first === second)? options.fn(this) : options.inverse(this);
 });
 
+// Views
 var partialDir = __dirname + '/views/partials';
 if(app.get('env') === 'development') {
 	hbsutils.registerWatchedPartials(partialDir);
@@ -52,9 +37,8 @@ if(app.get('env') === 'development') {
 	hbsutils.registerPartials(partialDir, {precompile: true});
 }
 
-var controllers = require('./controllers')(router);
-
-app.use('/', router);
+// Serve
+controllers(router);
 app.listen(port, function() {
 	console.log('Masterdex booting in [' + app.get('env') + '] on :' + port);
 });
--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,3 +1,17 @@
+var _ 			= require('lodash-node'),
+	Negotiator 	= require('negotiator'),
+	mediaTypes 	= ['text/html', 'application/json'];
+
+/**
+ * An object defining the properties we want to have publicly exposed
+ * @type {Object}
+ */
+var proto = {
+	name: '',
+	dexNumber: '',
+	description: ''
+};
+
 function PokemonController(){}
 
 PokemonController.prototype.register = function register(router) {
@@ -9,8 +23,70 @@ PokemonController.prototype.register = function register(router) {
 	router.delete('/pokemon/:id', this.remove);
 };
 
+/**
+ * Provides a convenience method to create an object with public properties
+ * from any given object
+ * @param  {Object} entity The entity who's properties we want to properties we want to take
+ * @return {Object}        An object with publicly-allowable properties
+ */
+PokemonController.prototype.buildPayload = function buildPayload(entity) {
+	var payload = _(proto).clone();
+	_(payload).keys().each(function(key) {
+		payload[key] = entity[key];
+	});
+	return payload;
+};
+
+/**
+ * Common rendering code
+ * @param  {[type]} req      [description]
+ * @param  {[type]} res      [description]
+ * @param  {[type]} entity   The raw entity to provide to the template
+ * @param  {[type]} template The template to render
+ * @return {[type]}          [description]
+ */
+PokemonController.prototype.render = function render(req, res, entity, template) {
+	var payload,
+		responseType = new Negotiator(req).mediaType(mediaTypes);
+
+	if(_.isArray(entity)) {
+		payload = [];
+		entity.forEach(function(el) {
+			payload.push(this.buildPayload(el));
+		}.bind(this));
+	} else {
+		payload = this.buildPayload(entity);
+	}
+
+	switch(responseType) {
+		case 'application/json':
+			res.json(payload);
+			break;
+		default:
+			if(req.method === 'GET') {
+				res.render('pokemon/show', payload);
+			} else {
+				res.redirect('/pokemon/' + ('00' + entity.dexNumber).slice(-3));
+			}
+			break;
+	}
+};
+
 PokemonController.prototype.index = function index(req, res) {
-	res.send('[GET] /pokemon');
+	var entities = [{
+			name: 'Bulbasaur',
+			dexNumber: 1,
+			description: 'The leaf pokemon'
+		}, {
+			name: 'Ivysaur',
+			dexNumber: 2,
+			description: 'The leaf pokemon'
+		}, {
+			name: 'Venusaur',
+			dexNumber: 3,
+			description: 'The leaf pokemon'
+		}];
+	this.render(req, res, entities, 'pokemon/index');
 };
 
 PokemonController.prototype.create = function create(req, res) {
@@ -18,11 +94,11 @@ PokemonController.prototype.create = function create(req, res) {
 };
 
 PokemonController.prototype.get = function get(req, res) {
-	res.locals.entity = {
-		'name': 'Bulbasaur',
-		'dexNumber': 1
-	}
-	res.locals.template = 'pokemon/show';
+	this.render(req, res, {
+		name: 'Bulbasaur',
+		dexNumber: 1,
+		description: 'The leaf pokemon'
+	});
 };
 
 PokemonController.prototype.update = function update(req, res) {
